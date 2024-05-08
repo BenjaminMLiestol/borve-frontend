88a3b947743070da9400461d1a448343e2393215
BenjaminMLiestol <benjamin@izy.no>
2024-05-08 13:58:27 +0200

    removed unused service

--- src/api/feedbackService.ts
+++
@@ -1,9 +0,0 @@
-import { client } from "@/api/client";
-import { ErrorFeedback } from "@/types/models/models";
-
-export const postFeedback = (data: ErrorFeedback) => client.post("feedback/", data);
-
-export const getFeedbackList = async () => {
-	const response = await client.get<ErrorFeedback[]>("feedback/list");
-	return response.data;
-};
